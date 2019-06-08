d928c63434da776c953fd192b29f855c1d63f1ed
Tudor Iordan <tudor.iordan@iquestgroup.com>
2019-06-09 01:47:31 +0300

    rateTranzaction

--- client/src/app/tranzaction-rating/tranzaction-rating.component.ts
+++ client/src/app/tranzaction-rating/tranzaction-rating.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {ApiService} from '../api.service';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-tranzaction-rating',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tranzaction-rating.component.scss']
 })
 export class TranzactionRatingComponent implements OnInit {
-
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+
+  ratingSubmit(ratingForm: NgForm) {
+    this.router.navigate(['']);
+    this.apiService.rateTransaction(ratingForm.value)
+      .subscribe((result) => {
+          console.log('result %o', result);
+        },
+      error => {
+        console.log('error: %o', error);
+      });
   }
 
 }
