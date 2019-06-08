0174dda7a27dd1c3844651951e6821305adfa315
Liviu Daraban <liviu.daraban@iquestgroup.com>
2019-06-08 13:04:11 +0300

    generated dashboard & login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Login' }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    data: { title: 'Dashboard' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
